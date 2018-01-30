4cc6e38ff8f67eb6f0f5ac251bf7cf21f1bc23c0
Alexander Donkov <abdonkov@gmail.com>
2018-01-31 01:56:36 +0200

    Added functionality for dice betting

--- OnlineCasinoWebClient/Scripts/betHandler.js
+++ OnlineCasinoWebClient/Scripts/betHandler.js
@@ -8,9 +8,82 @@
     document.getElementById("betAmount").innerText = betAmount;
 }
 
+function sendDiceBet() {
+    var betAmount = parseInt(document.getElementById("betAmount").innerText);
+    var selectedElement;
+    Array.from($('#bets-wrapper').children('div')).forEach(function (item) {
+        if ($(item).hasClass("active")) {
+            selectedElement = item.innerText;
+        }
+    });
+
+    if (typeof selectedElement != 'undefined') {
+
+        var betSum = parseInt(selectedElement);        
+        if (betAmount <= 0) {
+            alert("Invalid bet amount!");
+            return;
+        }
+        else if (betSum >= 2 && betSum <= 12) {
+
+            var userId = $("#userSecret").text();
+            var userToken = $("#userTSecret").text();
+
+            var betJson = {
+                "bet": betSum,
+                "stake": betAmount
+            };
+
+            
+            $.ajax({
+                url: domainName + "api/users/" + userId + "/dicebets",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(betJson),
+                headers: { "OnlineCasino-Token": userToken},
+                success: function (data) {
+                    //Magic
+                    var rollNumber = betSum;
+                    if (data.win == 0) {
+                        while (rollNumber == betSum) {
+                            rollNumber = Math.floor(Math.random() * 11) + 2;
+                        }
+                    }
+
+                    var dice1Num = 0;
+                    var dice2Num = 0;
+                    while (dice1Num + dice2Num != rollNumber) {
+                        dice1Num = Math.floor(Math.random() * 6) + 1;
+                        dice2Num = Math.floor(Math.random() * 6) + 1;
+                    }
+
+                    var won = !(data.win == 0);
+
+                    // Roll the dice
+                    showDiceResult(dice1Num, dice2Num, won);
+                },
+                error: function () { alert('Something went wrong!'); }
+            });
+
+            startRolling();
+        }
+        else {
+            alert("Fuck you!");
+        }
+    }
+    else {
+        alert("Please select dice sum!");
+    }
+}
+
+function makeDiceRoll(rollNumber) {
+    
+}
+
+
 function sendRouletteBet() {
     var betAmount = parseInt(document.getElementById("betAmount").innerText);
-    if (betAmount <= 0) { alert("Invalid bet amount"); return; }
+    if (betAmount <= 0) { alert("Invalid bet amount!"); return; }
     else {
         verifyBet();
         alert("Bet Sent");
