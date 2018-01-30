244cc77eeefb4245ac5030b29ca0e59404e2e2bd
Alexander Donkov <abdonkov@gmail.com>
2018-01-30 13:27:36 +0200

    Added Dice Controller

---
+++ OnlineCasinoWebClient/Controllers/DiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineCasinoWebClient.Controllers
+{
+    public class DiceController : Controller
+    {
+        // GET: Dice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
